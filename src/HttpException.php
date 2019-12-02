fffcd8b47a5d29425f51f7f14591309f3e0e01d9
andanyang <1218853253@qq.com>
2019-12-02 14:06:31 +0800

    first commit

---
+++ src/HttpException.php
@@ -0,0 +1,10 @@
+<?php
+//
+//namespace tests;
+//
+//use PHPUnit\Framework\ExpectationFailedException;
+//
+//class HttpException extends ExpectationFailedException
+//{
+//    //
+//}
---
+++ src/TestCase.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: yunwuxin <448901948@qq.com>
+// +----------------------------------------------------------------------
+namespace tests;
+
+use think\App;
+
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    protected $baseUrl = '';
+    use ApplicationTrait, AssertionsTrait, CrawlerTrait;
+
+    public function __construct(string $name = null, array $data = [], string $dataName = '')
+    {
+        // 引入需要的环境
+        require_once __DIR__ . '/../thinkphp/base.php';
+        // 初始化 App 对象，并将 APP_PATH 指向项目的 application 目录
+        App::getInstance()->path(__DIR__ . '/../application/')->initialize();
+
+        parent::__construct($name, $data, $dataName);
+    }
+}
